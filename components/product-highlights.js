fd7b7377cd05966859e49b53bcac90371e0bf822
wyandrew <andrewsit.wy@gmail.com>
2021-02-17 19:53:26 +0800

    Highlight component only show highlights. Changed base font weight to 350

--- components/product-highlights.js
+++ components/product-highlights.js
@@ -3,12 +3,16 @@ import styles from './product-highlights.module.css'
 import { labels } from '../siteConfig.json'
 
 const ProductHighlights = ({productList}) => {
+  console.log('p: ', productList)
   if (!productList) return null;
   return(
     <div className={styles.productHighlights}>
       <div className={styles.highlightHeader}>{labels.hk.prductHighlights}</div>
       <div className={styles.productWrapper}>
-        {productList.map((product)=>(<ProductCard key={product.slug} productProps={product.fm} slug={product.slug}/>))}
+        {productList.map((product)=>
+          { if (product.fm.highlight)
+            return <ProductCard key={product.slug} productProps={product.fm} slug={product.slug}/>}
+        )}
       </div>
     </div>
   )
