eb04f1f5c4f7a94c4aa7a007c8161262ab7c3c78
wyandrew <andrewsit.wy@gmail.com>
2021-02-20 02:34:00 +0800

    added package options to cart

--- blocks/product-card.js
+++ blocks/product-card.js
@@ -2,14 +2,13 @@ import CardColor from '../src/utils/cardColor'
 import styles from './product-card.module.css'
 
 const ProductCard = ({ productProps, slug }) => {
-const {pid, category, country, display_name, process, flavors_main, price_200g, image } = productProps;
+const {pid, category, country, display_name, process, flavors_main, price_200g, price_500g, price_1kg, price_dripbag, price_gb, image } = productProps;
 const flavorStr = flavors_main.reduce((str, flavor, i)=>{ //break flavors array into one line
   if (i < flavors_main.length -1) {
       return (str + flavor + ' - ')
     } else return str + flavor
   }, '');
   const color = CardColor(productProps);
-  console.log(display_name, ' : ', color);
   return (
     <div className={styles.productCard}>
       <div className='snipcart-add-item'
@@ -24,6 +23,10 @@ const flavorStr = flavors_main.reduce((str, flavor, i)=>{ //break flavors array 
         data-item-custom1-options="不需要研磨|粗 (手沖)|中 (Aeropress)|幼 (Espresso)"
         data-item-custom1-value="不需要研磨"
         data-item-custom1-required="true"
+        data-item-custom2-name="包裝/種類"
+        data-item-custom2-options={`200g|500g[+${price_500g-price_200g}]|1kg[+${price_1kg-price_200g}]|掛耳包[+${price_dripbag-price_200g}]|生豆[+${price_gb-price_200g}]`}
+        data-item-custom2-value="200g"
+        data-item-custom2-required="true"
       >
         <div className={styles.imageArea} style={{backgroundColor: color}}>
           <img className={styles.bgPattern} src='/patternBG_SO.svg' alt='background pattern' />
