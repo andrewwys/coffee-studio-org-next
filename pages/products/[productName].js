5b507a608e33614b85becb97264496940b23b730
wyandrew <andrewsit.wy@gmail.com>
2021-03-21 21:55:10 +0800

    fixed dynamic sticky cart button on product-details page

--- pages/products/[productName].js
+++ pages/products/[productName].js
@@ -13,9 +13,32 @@ import CardPatternPicker from '../../src/utils/card-pattern-picker'
 import { labels, display } from '../../siteConfig.json'
 import styles from './[productName].module.css'
 
+import  { useEffect, useRef, useState } from 'react';
+
 const ProductDetails = ({fm}) => {
   const { details } = labels;
   const {lang} = useAppContext();
+
+  const [isSticky, setSticky] = useState(true);
+  const ref1 = useRef(null); //add to cart link
+  const ref2 = useRef(null); //add to cart icon
+  const handleScroll = () => {
+    const y = window.pageYOffset;
+    const w = window.innerHeight;
+    if (ref1.current) {
+      setSticky(ref1.current.offsetTop >= w + y - 100);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    console.log('handle scroll added.');
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+      console.log('handle scroll removed.');
+    };
+  }, []);
+  // console.log('is Sticky: ', isSticky);
   if (!fm) return <></>
   const { country, display_name, process, flavors_main, flavors_desc, description, altitude, varietal, roast_level, image, price_200g, price_500g, price_1kg, price_dripbag, price_gb, sweetness, acidity, mouthfeel, finish, floral, fruits, nuts, sugars, theme_color, category } = fm[lang];
   const flavorStr = flavorFormatter(flavors_main);
@@ -23,7 +46,6 @@ const ProductDetails = ({fm}) => {
   const patternStr = CardPatternPicker(category);
   return (
     <div>
-      
       <Layout>
         <div className={styles.wrapper}>
           <NavPath />
@@ -42,13 +64,39 @@ const ProductDetails = ({fm}) => {
             <InfoRow name={details[lang].price}>{`HKD ${price_200g} / ${price_500g} / ${price_1kg} / ${price_dripbag} / ${price_gb}`}</InfoRow>
           </div>
           <div className={styles.addToCart}>
-            <div className={styles.addToCartText} >add to cart &gt;</div>
-            <div className={styles.addToCartButton} ><AddToCartButton size='70' /></div>
+            <div className={ isSticky ? 'cart-button-sticky' : 'cart-button'} ref={ref2} ><AddToCartButton size='70' /></div>
+            <div className={ isSticky ? 'cart-link' : 'cart-link cart-fix'} ref={ref1} >add to cart &gt;</div>
+            
           </div>
         </div>
       </Layout>
+      <style jsx>{`
+        .cart-link {
+          text-decoration: underline;
+          font-size: var(--fsize-5);
+          color: var(--green-header);
+          padding-right: 175px;
+          text-align: right;
+          cursor: pointer;
+          float: right;
+        }
+        .cart-button-sticky {
+          position: fixed;
+          bottom: 10%;
+          right: 90px;
+          cursor: pointer;
+        }
+        .cart-fix {
+          padding-right: 15px;
+        }
+        .cart-button {
+          float: right;
+          padding-right: 90px;
+          position: relative;
+          bottom: 45px;
+        }
+      `}</style>
     </div>
-    
   );
 }
 
