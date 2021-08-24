21e84a6a30ef62c760ed884b2a2ef08d0b8f2cb5
andrewwy <andrewsit.wy@gmail.com>
2021-08-24 18:48:12 -0400

    changed UI for coffee classes banner

---
+++ components/coffee-class-banner-dup.jsx
@@ -0,0 +1,70 @@
+import Link from 'next/link'
+import { labels } from '../siteConfig.json'
+import { useAppContext } from '../src/context/state'
+
+const CoffeeClassBanner = () => {
+  const { lang } = useAppContext();
+  return (
+    <div className='main'>
+      <div className='tag'>{`${labels[lang].coffeeClasses}`}</div>
+      <Link href='/coffee-classes'><a className='link'>{`${labels[lang].learnMore} >`}</a></Link>
+      <style jsx>{`
+        .main {
+          margin: 50px 0;
+          height: 100vh;
+          white-space: nowrap;
+          position: relative;
+        }
+        .main::before {
+          position: absolute;
+          content: "";
+          top: 0; left: 0;
+          width: 100%; height: 100%;
+          background-image: url('/images/coffee-class-banner.jpg');
+          background-repeat: no-repeat;
+          background-position: center;
+          background-size: cover;
+          opacity: 0.8;
+          transition: all 0.2s ease;
+        }
+        .main:hover::before {
+          opacity: 0.9;
+        }
+        .tag {
+          display: inline-block;
+          position: relative;
+          top: 5%;
+          width: 40%;
+          height: 128px;
+          background-image: url('/images/fading-tag-brown.png');
+          background-repeat: no-repeat;
+          background-position: right top;
+          background-size: cover;
+          text-indent: 50px;
+          color: white;
+          font-size: var(--fsize-3);
+          line-height: 128px;
+        }
+        .link {
+          position: relative;
+          top: 70%;
+          font-size: var(--fsize-4);
+          padding: 10px 20px;
+          color: white;
+          display: inline;
+          border: 5px solid #C1882F;
+          background-color: #C1882F;
+          border-radius: 5px;
+          box-shadow: 6px 6px 9px rgb(0 0 0 / 75%);
+          transition: all 0.1s ease;
+        }
+        .link:hover {
+          border-width: 6px;
+          font-weight: 400;
+        }
+        `}</style>
+    </div>
+  );
+}
+
+export default CoffeeClassBanner;
--- components/coffee-class-banner.jsx
+++ components/coffee-class-banner.jsx
@@ -1,66 +1,88 @@
 import Link from 'next/link'
-import { labels } from '../siteConfig.json'
+import { labels, classes } from '../siteConfig.json'
 import { useAppContext } from '../src/context/state'
 
 const CoffeeClassBanner = () => {
   const { lang } = useAppContext();
   return (
     <div className='main'>
-      <div className='tag'>{`${labels[lang].coffeeClasses}`}</div>
-      <Link href='/coffee-classes'><a className='link'>{`${labels[lang].learnMore} >`}</a></Link>
+      <img className='image' src='/images/coffee-class_banner_2.png' alt='coffee class' />
+      <div className='circle'>
+        <text className='join'>{classes[lang].joinOurClasses}</text>
+        <Link href='/coffee-classes'><a className='button'>{labels[lang].learnMore}</a></Link>
+      </div>
+      
       <style jsx>{`
         .main {
-          margin: 50px 0;
-          height: 100vh;
+          margin: 50px 92px 50px 0;
           white-space: nowrap;
           position: relative;
+          padding-bottom: 60px;
         }
-        .main::before {
+        .image {
+          position: relative;
+          width: 58vw;
+          z-index: 10;
+        }
+        .circle {
           position: absolute;
-          content: "";
-          top: 0; left: 0;
-          width: 100%; height: 100%;
-          background-image: url('/images/coffee-class-banner.jpg');
-          background-repeat: no-repeat;
-          background-position: center;
-          background-size: cover;
-          opacity: 0.8;
-          transition: all 0.2s ease;
-        }
-        .main:hover::before {
-          opacity: 0.9;
-        }
-        .tag {
-          display: inline-block;
-          position: relative;
-          top: 5%;
-          width: 40%;
-          height: 128px;
-          background-image: url('/images/fading-tag-brown.png');
-          background-repeat: no-repeat;
-          background-position: right top;
-          background-size: cover;
-          text-indent: 50px;
-          color: white;
-          font-size: var(--fsize-3);
-          line-height: 128px;
-        }
-        .link {
-          position: relative;
-          top: 70%;
-          font-size: var(--fsize-4);
-          padding: 10px 20px;
-          color: white;
-          display: inline;
-          border: 5px solid #C1882F;
-          background-color: #C1882F;
-          border-radius: 5px;
-          box-shadow: 6px 6px 9px rgb(0 0 0 / 75%);
-          transition: all 0.1s ease;
-        }
-        .link:hover {
+          top: 80px;
+          left: 45vw;
+          background: transparent;
+          border-color: var(--gold-subtitle);
+          border-style: solid;
           border-width: 6px;
-          font-weight: 400;
+          border-radius: 50%;
+          width: 42vw;
+          height: 42vw;
+          z-index: 9;
+        }
+        .join {
+          position: absolute;
+          top: 60px;
+          left: 30%;
+          width: 370px;
+          inline-size: 20vw;
+          white-space: normal;
+          font-size: 3.2vw;
+          color: var(--gold-subtitle);
+        }
+        .button {
+          position: absolute;
+          width: 16vw;
+          height: 3.3vw;
+          right: -4.5vw;
+          top: 25.8vw;
+          font-size: 2vw;
+          padding: 0.7% 0;
+          background-color: var(--gold-subtitle);
+          color: white;
+          border-radius: 50px;
+          text-align: center;
+          transition: all 0.2s
+        }
+        .button:hover {
+          transform: scale(1.05);
+        }
+        .button:active {
+          right: -6vw
+        }
+        @media screen and (max-width: 470px) {
+          .circle {
+            border-color: transparent;
+          }
+          .join {
+            top: -50px;
+            font-size: 1.3rem;
+            width: 140px;
+          }
+          .button {
+            width: 150px;
+            height: 33px;
+            font-size: 1.1rem;
+            left: 60px;
+            top: 30px;
+          }
         }
         `}</style>
     </div>
